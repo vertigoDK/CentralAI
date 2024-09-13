aadb07aea94a5b6bdd4f0e49a842e8af7017f988
Denis Kushnaryov <denis.k.kushnaryov@gmail.com>
2024-09-13 20:14:51 +0500

    gemini ai

--- services/ai_handlers/gemini/ai_handlers.py
+++ services/ai_handlers/gemini/ai_handlers.py
@@ -1,3 +1,10 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
 
 class GeminiAI:
-    pass
+    def __init__(self):
+        genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+        self._model = genai.GenerativeModel("gemini-1.5-flash")
+
